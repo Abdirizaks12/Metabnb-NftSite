63baa40260710136851b7e4d478a649135113fce
Abdirizak <abdirizaksigat99@gmail.com>
2022-11-15 01:47:07 -0800

    centered the divs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Homepage from "./Homepage";
 import PlaceToStay from "./PlaceToStay";
 import { BrowserRouter as Router, Route,  Routes}  from "react-router-dom";
-
+import "./app.css"
 export default function App(){
     return (
+      <div className="app-view">
         <Router>
           <Routes>
             <Route exact path="/" element={<Homepage />} />
             <Route path="/PlaceToStay" element={<PlaceToStay />} />
           </Routes>
-        </Router> )
+        </Router>
+        </div>
+         )
 }
