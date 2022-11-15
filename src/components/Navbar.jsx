3e16de88c0dd63347d9fd5f6ab9086870f43caf7
Abdirizak <abdirizaksigat99@gmail.com>
2022-11-14 23:58:42 -0800

    designing  web responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,26 @@ import {Link} from "react-router-dom"
 
 export default function Navbar(){
     const[openModal,setOpenModal] = React.useState(false)
+    const[toggleMenu , setToggleMenu]=React.useState(false)
+
+    const toggleNav = ()=>{
+        setToggleMenu(!toggleMenu)
+    }
+    
     return(
     <div className="navbar">
         <img src="./logo.png"/>
-        <ul className="menu">
+       <nav> 
+        { toggleMenu && (<ul className="menu">
             <Link >Home</Link>
             <Link to="./PlaceToStay">Place to stay</Link> 
             <a href="#">NFTs</a>
             <a href="#">Community</a>
-        </ul>
+        </ul>)}
+        <button onClick={toggleNav} className="menu-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg></button>
+       </nav> 
         <button onClick={()=>setOpenModal(true)} className="connect-btn">Connect wallet</button>  
         {openModal && <Modal closeModal={setOpenModal}/>}
    </div>
